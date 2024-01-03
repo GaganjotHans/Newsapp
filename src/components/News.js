e259a98c8c3daaef536bb54f55d19626cf4b5cfa
GaganjotHans <109852605+GaganjotHans@users.noreply.github.com>
2024-01-03 17:37:56 -0500

    populate data using next and previous button

--- src/components/News.js
+++ src/components/News.js
@@ -2,21 +2,51 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
-  
   constructor() {
     super();
     this.state = {
       articles: [],
+      page: 1,
       loading: false,
     };
   }
-async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=68f4bec107164e3584b98c2c6cac958e";
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=us&apiKey=68f4bec107164e3584b98c2c6cac958e&page=1&pageSize=9";
     let data = await fetch(url);
     let parseData = await data.json();
-    this.setState({articles: parseData.articles})
+    this.setState({
+      articles: parseData.articles,
+      totalResults: parseData.totalResults,
+    });
+  }
 
-}
+  handlePrevClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=68f4bec107164e3584b98c2c6cac958e&page=${
+      this.state.page - 1
+    }&pageSize=9`;
+    let data = await fetch(url);
+    let parseData = await data.json();
+    this.setState({
+      page: this.state.page - 1,
+      articles: parseData.articles,
+    });
+  };
+
+  handleNextClick = async () => {
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 9)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=68f4bec107164e3584b98c2c6cac958e&page=${
+        this.state.page + 1
+      }&pageSize=9`;
+      let data = await fetch(url);
+      let parseData = await data.json();
+      this.setState({
+        page: this.state.page + 1,
+        articles: parseData.articles,
+      });
+    }
+  };
 
   render() {
     return (
@@ -26,11 +56,37 @@ async componentDidMount(){
           {this.state.articles.map((element) => {
             return (
               <div className="col-md-4" key={element.url}>
-                <NewsItem  title={element.title?element.title:""} description={element.description?element.description:"The description is now available right now.Sorry for the inconvenience!"} imageUrl={
-                element.urlToImage} newsUrl={element.url}/>
+                <NewsItem
+                  title={element.title ? element.title : ""}
+                  description={
+                    element.description
+                      ? element.description
+                      : "The description is now available right now.Sorry for the inconvenience!"
+                  }
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
               </div>
             );
           })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePrevClick}
+          >
+            {" "}
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,12 +5,25 @@ export class NewsItem extends Component {
     let { title, description, imageUrl, newsUrl } = this.props;
     return (
       <div className="my-3">
-        <div className="card" style={{width: "18rem"}}>
-          <img src={!imageUrl?"https://images.foxweather.com/static.foxweather.com/www.foxweather.com/content/uploads/2023/06/1024/512/GettyImages-1242931997.jpg?ve=1&tl=1":imageUrl} className="card-img-top" alt="..." />
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src={
+              !imageUrl
+                ? "https://images.foxweather.com/static.foxweather.com/www.foxweather.com/content/uploads/2023/06/1024/512/GettyImages-1242931997.jpg?ve=1&tl=1"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
-            <a href={newsUrl} target="_blank" className="btn btn-primary btn-sm">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-dark btn-sm"
+            >
               Read More
             </a>
           </div>
