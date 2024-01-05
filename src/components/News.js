aab5b5cf912cc25a63a9193019ec37e1c7f16777
GaganjotHans <109852605+GaganjotHans@users.noreply.github.com>
2024-01-05 10:47:11 -0500

    Spinner Added

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
 
 export class News extends Component {
   constructor() {
@@ -13,11 +14,13 @@ export class News extends Component {
   async componentDidMount() {
     let url =
       "https://newsapi.org/v2/top-headlines?country=us&apiKey=68f4bec107164e3584b98c2c6cac958e&page=1&pageSize=9";
+      this.setState({loading: true});
     let data = await fetch(url);
     let parseData = await data.json();
     this.setState({
       articles: parseData.articles,
       totalResults: parseData.totalResults,
+      loading:false
     });
   }
 
@@ -25,25 +28,28 @@ export class News extends Component {
     let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=68f4bec107164e3584b98c2c6cac958e&page=${
       this.state.page - 1
     }&pageSize=9`;
+    this.setState({loading: true});
     let data = await fetch(url);
     let parseData = await data.json();
     this.setState({
       page: this.state.page - 1,
       articles: parseData.articles,
+      loading: false
     });
   };
 
   handleNextClick = async () => {
-    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 9)) {
-    } else {
+    if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / 9))) {
       let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=68f4bec107164e3584b98c2c6cac958e&page=${
         this.state.page + 1
       }&pageSize=9`;
+      this.setState({loading: true});
       let data = await fetch(url);
       let parseData = await data.json();
       this.setState({
         page: this.state.page + 1,
         articles: parseData.articles,
+        loading:false
       });
     }
   };
@@ -52,8 +58,9 @@ export class News extends Component {
     return (
       <div className="container my-3">
         <h2>Treding News</h2>
+        {this.state.loading && <Spinner/>}
         <div className="row">
-          {this.state.articles.map((element) => {
+          {!this.state.loading && this.state.articles.map((element) => {
             return (
               <div className="col-md-4" key={element.url}>
                 <NewsItem
@@ -81,6 +88,7 @@ export class News extends Component {
             &larr; Previous
           </button>
           <button
+          disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 9)}
             type="button"
             className="btn btn-dark"
             onClick={this.handleNextClick}
